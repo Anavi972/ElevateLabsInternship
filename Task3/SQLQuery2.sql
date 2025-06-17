31b4e60f6ff2c85ff16482ad8c4f4be7ddef0eed
Anavi972 <155766306+Anavi972@users.noreply.github.com>
2025-06-17 22:37:39 +0530

    Update SQLQuery2.sql

--- Task3/SQLQuery2.sql
+++ Task3/SQLQuery2.sql
@@ -31,3 +31,17 @@ select (select max(amount) from details) as max_amount from Details;
 select max(max_amt) as maximum
 from (select category,max(amount) as max_amt,min(amount) as min_amt,count(category) as count_category
 from details group by category) as agg_table;
+
+drop index idxx_category on details;
+drop index idx_category on details;
+
+create index idx_category
+on details(category);
+select category,avg(amount) from details group by Category;
+
+create unique index uni_order on orders(order_id);
+select order_id from orders;
+
+create index idx_cat_subcat 
+on details(category,sub_category);
+select category,sub_category from details group by Category,Sub_Category ;
