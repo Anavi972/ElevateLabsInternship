7b7978d5b5e07154fbbb53ba4265d283fb6e35b7
Anavi972 <155766306+Anavi972@users.noreply.github.com>
2025-06-17 21:14:49 +0530

    Update SQLQuery2.sql

--- Task3/SQLQuery2.sql
+++ Task3/SQLQuery2.sql
@@ -17,3 +17,17 @@ right join dbo.Orders
 on Details.Order_ID = Orders.Order_ID
 order by details.Order_ID;
 
+select customername from Orders where
+Order_ID in (select Order_ID from Details where Sub_Category = 'Chairs');
+
+select customername, Sub_Category from Orders as o inner join Details as d
+on o.Order_ID = d.Order_ID
+where Sub_Category = 'Chairs';
+
+select (select avg(amount) from Details) as average_amt from details;
+
+select (select max(amount) from details) as max_amount from Details;
+
+select max(max_amt) as maximum
+from (select category,max(amount) as max_amt,min(amount) as min_amt,count(category) as count_category
+from details group by category) as agg_table;
